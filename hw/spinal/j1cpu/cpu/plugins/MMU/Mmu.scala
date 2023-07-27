321f26ab99001b9a4be9f5786ad926852d16e489
zzyNorthPole <2422588629@qq.com>
2023-07-27 13:54:11 +0800

    add mmu and tlb support

--- hw/spinal/j1cpu/cpu/plugins/MMU/Mmu.scala
+++ hw/spinal/j1cpu/cpu/plugins/MMU/Mmu.scala
@@ -1,3 +1,110 @@
-package j1cpu.cpu.plugins.MMU class MMU {
+package j1cpu.cpu.plugins.MMU
 
+import j1cpu.cpu.TlbConfig
+import spinal.core.internals.Operator.UInt
+import spinal.core._
+import spinal.lib.MuxOH
+
+class Mmu(config: TlbConfig) extends Component {
+  val io = new Bundle {
+    val en = in Bool()
+    val virtAddr = in UInt (32 bits)
+    val phyAddr = out UInt (32 bits)
+    val k0Cached = in Bool()
+    val cached = out Bool()
+
+    val tlb = new Bundle {
+      val VPN = out UInt(20 bits)
+      val din = in(new TLBResult)
+      val hit = in Bool()
+    }
+
+    val tlbException = new Bundle {
+      val refill = out Bool()
+      val invalid = out Bool()
+      val modified = out Bool()
+    }
+  }
+  noIoPrefix()
+
+  import io._
+
+  val virtual = new Bundle {
+    val useg = U"32'h00000000" // user mapped
+    val kseg0 = U"32'h80000000" // kernel unmapped
+    val kseg1 = U"32'hA0000000" // kernel unmapped uncached
+    val ksseg = U"32'hC0000000" // supervisor mapped
+    val kseg3 = U"32'hE0000000" // kernel mapped
+  }
+  val physical = new Bundle {
+    val useg = U"32'h00000000"
+    val kseg0 = U"32'h00000000"
+    val kseg1 = U"32'h00000000"
+    val ksseg = U"32'hC0000000"
+    val kseg3 = U"32'hE0000000"
+  }
+  val unmapped = virtAddr >= virtual.kseg0 && virtAddr < virtual.kseg1
+  val unmappedUncached = virtAddr >= virtual.kseg1 && virtAddr < virtual.ksseg
+  val mapped = ~unmapped && ~unmappedUncached
+  phyAddr := MuxOH(
+    Vec(
+      unmapped,
+      unmappedUncached,
+      mapped
+    ),
+    Vec(
+      virtAddr - virtual.kseg0 + physical.kseg0,
+      virtAddr - virtual.kseg1 + physical.kseg1,
+      if (config.use) (tlb.din.PFN @@ virtAddr(11 downto 0)) else virtAddr
+    )
+  )
+  cached := MuxOH(
+    Vec(
+      unmapped,
+      unmappedUncached,
+      mapped
+    ),
+    Vec(
+      k0Cached,
+      False,
+      tlb.din.C === U(3, 3 bits)
+    )
+  )
+  tlb.VPN := (if (config.use) virtAddr(31 downto 12) else U(0, 20 bits))
+  tlbException.refill := en && MuxOH(
+    Vec(
+      unmapped,
+      unmappedUncached,
+      mapped
+    ),
+    Vec(
+      False,
+      False,
+      if (config.use) !tlb.hit else False
+    )
+  )
+  tlbException.invalid := en && MuxOH(
+    Vec(
+      unmapped,
+      unmappedUncached,
+      mapped
+    ),
+    Vec(
+      False,
+      False,
+      if (config.use) (tlb.hit && !tlb.din.V) else False
+    )
+  )
+  tlbException.modified := en && MuxOH(
+    Vec(
+      unmapped,
+      unmappedUncached,
+      mapped
+    ),
+    Vec(
+      False,
+      False,
+      if (config.use) (tlb.hit && tlb.din.V && !tlb.din.D) else False
+    )
+  )
 }
--- hw/spinal/j1cpu/cpu/plugins/MMU/Tlb.scala
+++ hw/spinal/j1cpu/cpu/plugins/MMU/Tlb.scala
@@ -1,3 +1,136 @@
-package j1cpu.cpu.plugins.MMU class TLB {
+package j1cpu.cpu.plugins.MMU
 
+import j1cpu.cpu.{J1cpuConfig, TlbConfig}
+import spinal.core._
+import spinal.lib._
+
+// mips32 release 1 only support 4kb page
+class TLBRequest extends Bundle {
+  val VPN = UInt(20 bits)
+  val ASID = UInt(8 bits)
+}
+class TLBResult extends Bundle {
+  val PFN = UInt(20 bits)
+  val C = UInt(3 bits)
+  val D = Bool()
+  val V = Bool()
+}
+class TLBItem extends Bundle {
+  val VPN2 = UInt(19 bits)
+  val ASID = UInt(8 bits)
+  val G = Bool()
+  val PFN0, PFN1 = UInt(20 bits)
+  val C0, C1 = UInt(3 bits)
+  val D0, D1 = Bool()
+  val V0, V1 = Bool()
+}
+class TLB(config: TlbConfig) extends Component {
+  val io = new Bundle {
+    val wPorts = new Bundle {
+      val en = in Bool()
+      val addr = in UInt(log2Up(config.lines) bits)
+      val din = in(new TLBItem)
+    }
+    val rPorts = new Bundle {
+      val addr = in UInt(log2Up(config.lines) bits)
+      val dout = out(new TLBItem)
+    }
+    val queryPorts = Vec(
+      new Bundle {
+        val din = in(new TLBRequest)
+        val hit = out Bool()
+        val addr = out UInt(log2Up(config.lines) bits)
+        val dout = out(new TLBResult)
+      },
+      2
+    )
+  }
+  noIoPrefix()
+
+  import io._
+
+  if (config.use) {
+    val tlb = Vec(Reg(new TLBItem), config.lines)
+    for (i <- 0 until config.lines) {
+      tlb(i).VPN2 init U(19 bits, default -> True)
+      tlb(i).ASID init U(0, 8 bits)
+      tlb(i).G init False
+      tlb(i).PFN0 init U(0, 20 bits)
+      tlb(i).PFN1 init U(0, 20 bits)
+      tlb(i).C0 init U(0, 3 bits)
+      tlb(i).C1 init U(0, 3 bits)
+      tlb(i).D0 init False
+      tlb(i).D1 init False
+      tlb(i).V0 init False
+      tlb(i).V1 init False
+    }
+    val hits = Vec(Vec(Bool(), config.lines), 2)
+
+    for (i <- 0 until 2) {
+      for (j <- 0 until config.lines) {
+        hits(i)(j) := (tlb(j).VPN2 === queryPorts(i).din.VPN(19 downto 1)) && ((tlb(j).ASID === queryPorts(i).din.ASID) || tlb(j).G)
+      }
+      queryPorts(i).hit := hits(i).orR
+    }
+
+    for (i <- 0 until 2) {
+      queryPorts(i).addr := MuxOH(
+        hits(i),
+        for (j <- 0 until config.lines) yield U(j, log2Up(config.lines) bits)
+      )
+      queryPorts(i).dout.PFN := MuxOH(
+        hits(i),
+        for (j <- 0 until config.lines) yield (queryPorts(i).din.VPN(0) ? tlb(j).PFN1 | tlb(j).PFN0)
+      )
+      queryPorts(i).dout.C := MuxOH(
+        hits(i),
+        for (j <- 0 until config.lines) yield (queryPorts(i).din.VPN(0) ? tlb(j).C1 | tlb(j).C0)
+      )
+      queryPorts(i).dout.D := MuxOH(
+        hits(i),
+        for (j <- 0 until config.lines) yield (queryPorts(i).din.VPN(0) ? tlb(j).D1 | tlb(j).D0)
+      )
+      queryPorts(i).dout.V := MuxOH(
+        hits(i),
+        for (j <- 0 until config.lines) yield (queryPorts(i).din.VPN(0) ? tlb(j).V1 | tlb(j).V0)
+      )
+    }
+
+    when(wPorts.en) {
+      tlb(wPorts.addr) := wPorts.din
+    }
+
+    rPorts.dout := tlb(rPorts.addr)
+  }
+  else {
+    rPorts.dout.VPN2 := U(0, 19 bits)
+    rPorts.dout.ASID := U(0, 8 bits)
+    rPorts.dout.G := False
+    rPorts.dout.PFN0 := U(0, 20 bits)
+    rPorts.dout.PFN1 := U(0, 20 bits)
+    rPorts.dout.C0 := U(0, 3 bits)
+    rPorts.dout.C1 := U(0, 3 bits)
+    rPorts.dout.D0 := False
+    rPorts.dout.D1 := False
+    rPorts.dout.V0 := False
+    rPorts.dout.V1 := False
+    for (i <- 0 to 1) {
+      queryPorts(i).hit := True
+      queryPorts(i).addr := U(0, log2Up(config.lines) bits)
+      queryPorts(i).dout.PFN := U(0, 20 bits)
+      queryPorts(i).dout.C := U(0, 3 bits)
+      queryPorts(i).dout.D := False
+      queryPorts(i).dout.V := False
+    }
+  }
+}
+
+object TLBGen {
+  def main(args: Array[String]): Unit = {
+    val spinalConfig = SpinalConfig(
+      targetDirectory = "hw/gen",
+      defaultConfigForClockDomains = J1cpuConfig().clockConfig
+    )
+    spinalConfig.generateVerilog(new TLB(TlbConfig(true, 8)))
+  }
 }
