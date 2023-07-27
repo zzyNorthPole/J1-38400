6c5a60e3fe8132c0e9b836ca4aee90fb2102cd27
zzyNorthPole <2422588629@qq.com>
2023-07-27 13:58:33 +0800

    init commit j1cpu config

--- hw/spinal/j1cpu/cpu/J1cpuConfig.scala
+++ hw/spinal/j1cpu/cpu/J1cpuConfig.scala
@@ -3,6 +3,21 @@ package j1cpu.cpu
 import spinal.core._
 import spinal.lib.bus.amba4.axi.Axi4Config
 
+case class RegFillConfig(
+  rPorts: Int,
+  wPorts: Int
+                        ) {
+
+}
+
+case class ByPassConfig(
+  decoderPass: Int,
+  backendStage: Int,
+  backendUnfinishedStage:Int,
+  backendPass: Int
+                       ) {
+
+}
 case class CacheConfig(
   ways: Int,
   lines: Int,
@@ -21,24 +36,40 @@ case class TlbConfig(
 
 }
 
-class J1cpuConfig {
+case class J1cpuConfig() {
+  val sim = 1
+
   val clockConfig = ClockDomainConfig(
     resetActiveLevel = HIGH,
     resetKind = SYNC,
     clockEdge = RISING
   )
 
+  val regFillConfig = RegFillConfig(
+    rPorts = 2,
+    wPorts = 1
+  )
+
+  val byPassConfig = ByPassConfig(
+    decoderPass = 2,
+    backendStage = 4,
+    backendUnfinishedStage = 2,
+    backendPass = 1
+  )
+
   val axiConfig = Axi4Config(
     addressWidth = 32,
     dataWidth = 32,
-    idWidth = 5,
+    idWidth = 4,
     useRegion = false,
-    useQos = false
+    useQos = false,
+    useLock = false
   )
+
   val cacheConfig = CacheConfig(
     ways = 2,
-    lines = 1 << 20,
-    blockSize = 32
+    lines = 1 << 8,
+    blockSize = 16
   )
   val tlbConfig = TlbConfig(
     use = true,
